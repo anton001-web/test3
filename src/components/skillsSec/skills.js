3ae89042f3b39d7b5742897c4e5d3b42905914c8
Anton <you@example.com>
2024-09-17 22:58:54 +0300

    fix

--- src/components/skillsSec/skills.js
+++ src/components/skillsSec/skills.js
@@ -40,43 +40,13 @@ export const skills = [
     rate: 5,
   },
   {
-    img: "../assets/images/1.png",
-    title: "Tinkoff",
+    img: "../assets/images/9.png",
+    title: "Акселератор Возможностей ",
     rate: 5,
   },
   {
-    img: "../assets/images/2.jpeg",
-    title: "MIR pay",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/3.png",
-    title: "Hello World",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/4.jpeg",
-    title: "IBS",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/5.png",
-    title: "FB Group",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/6.png",
-    title: "Открытие банк",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/7.png",
-    title: "Ivi",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/8.png",
-    title: "ТрансТехСервис",
+    img: "../assets/images/10.png",
+    title: "Spatium Software",
     rate: 5,
   },
   {
@@ -120,43 +90,13 @@ export const skills = [
     rate: 5,
   },
   {
-    img: "../assets/images/1.png",
-    title: "Tinkoff",
+    img: "../assets/images/9.png",
+    title: "Акселератор Возможностей ",
     rate: 5,
   },
   {
-    img: "../assets/images/2.jpeg",
-    title: "MIR pay",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/3.png",
-    title: "Hello World",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/4.jpeg",
-    title: "IBS",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/5.png",
-    title: "FB Group",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/6.png",
-    title: "Открытие банк",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/7.png",
-    title: "Ivi",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/8.png",
-    title: "ТрансТехСервис",
+    img: "../assets/images/10.png",
+    title: "Spatium Software",
     rate: 5,
   },
   {
@@ -200,43 +140,13 @@ export const skills = [
     rate: 5,
   },
   {
-    img: "../assets/images/1.png",
-    title: "Tinkoff",
+    img: "../assets/images/9.png",
+    title: "Акселератор Возможностей ",
     rate: 5,
   },
   {
-    img: "../assets/images/2.jpeg",
-    title: "MIR pay",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/3.png",
-    title: "Hello World",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/4.jpeg",
-    title: "IBS",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/5.png",
-    title: "FB Group",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/6.png",
-    title: "Открытие банк",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/7.png",
-    title: "Ivi",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/8.png",
-    title: "ТрансТехСервис",
+    img: "../assets/images/10.png",
+    title: "Spatium Software",
     rate: 5,
   },
   {
@@ -280,43 +190,13 @@ export const skills = [
     rate: 5,
   },
   {
-    img: "../assets/images/1.png",
-    title: "Tinkoff",
+    img: "../assets/images/9.png",
+    title: "Акселератор Возможностей ",
     rate: 5,
   },
   {
-    img: "../assets/images/2.jpeg",
-    title: "MIR pay",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/3.png",
-    title: "Hello World",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/4.jpeg",
-    title: "IBS",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/5.png",
-    title: "FB Group",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/6.png",
-    title: "Открытие банк",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/7.png",
-    title: "Ivi",
-    rate: 5,
-  },
-  {
-    img: "../assets/images/8.png",
-    title: "ТрансТехСервис",
+    img: "../assets/images/10.png",
+    title: "Spatium Software",
     rate: 5,
   },
   {
@@ -357,6 +237,216 @@ export const skills = [
   {
     img: "../assets/images/8.png",
     title: "ТрансТехСервис",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/9.png",
+    title: "Акселератор Возможностей ",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/10.png",
+    title: "Spatium Software",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/1.png",
+    title: "Tinkoff",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/2.jpeg",
+    title: "MIR pay",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/3.png",
+    title: "Hello World",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/4.jpeg",
+    title: "IBS",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/5.png",
+    title: "FB Group",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/6.png",
+    title: "Открытие банк",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/7.png",
+    title: "Ivi",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/8.png",
+    title: "ТрансТехСервис",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/9.png",
+    title: "Акселератор Возможностей ",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/10.png",
+    title: "Spatium Software",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/1.png",
+    title: "Tinkoff",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/2.jpeg",
+    title: "MIR pay",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/3.png",
+    title: "Hello World",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/4.jpeg",
+    title: "IBS",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/5.png",
+    title: "FB Group",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/6.png",
+    title: "Открытие банк",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/7.png",
+    title: "Ivi",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/8.png",
+    title: "ТрансТехСервис",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/9.png",
+    title: "Акселератор Возможностей ",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/10.png",
+    title: "Spatium Software",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/1.png",
+    title: "Tinkoff",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/2.jpeg",
+    title: "MIR pay",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/3.png",
+    title: "Hello World",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/4.jpeg",
+    title: "IBS",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/5.png",
+    title: "FB Group",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/6.png",
+    title: "Открытие банк",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/7.png",
+    title: "Ivi",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/8.png",
+    title: "ТрансТехСервис",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/9.png",
+    title: "Акселератор Возможностей ",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/10.png",
+    title: "Spatium Software",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/1.png",
+    title: "Tinkoff",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/2.jpeg",
+    title: "MIR pay",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/3.png",
+    title: "Hello World",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/4.jpeg",
+    title: "IBS",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/5.png",
+    title: "FB Group",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/6.png",
+    title: "Открытие банк",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/7.png",
+    title: "Ivi",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/8.png",
+    title: "ТрансТехСервис",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/9.png",
+    title: "Акселератор Возможностей ",
+    rate: 5,
+  },
+  {
+    img: "../assets/images/10.png",
+    title: "Spatium Software",
     rate: 5,
   },
 ];
